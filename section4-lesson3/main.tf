af619b3a2ded323a8f77315ce715c70e12f34a4b
ocarvajarh <mykantelle@gmail.com>
2022-09-23 17:31:20 -0500

    add link about amazon-linux-ami with ssm

--- section4-lesson3/main.tf
+++ section4-lesson3/main.tf
@@ -3,13 +3,14 @@ provider "aws" {
 }
 
 #Get Linux AMI ID using SSM Parameter endpoint in us-east-1
+#https://aws.amazon.com/blogs/compute/query-for-the-latest-amazon-linux-ami-ids-using-aws-systems-manager-parameter-store/
 data "aws_ssm_parameter" "linuxAmi" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
 #Create and bootstrap EC2 in us-east-1
 resource "aws_instance" "ec2-vm" {
-  ami                         = data.aws_ssm_parameter.linuxAmi.value
+  ami                         = data.aws_ssm_parameter.linuxAmi.name
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
